7a3d222990dac5579515fd7e45bb6dff155dd673
AAashik519 <64531121+AAashik519@users.noreply.github.com>
2021-03-04 07:22:08 +0600

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,59 @@
+ 
+
+
+let Sdata =[
+    {
+        sname: 'Dark',
+        imgsrc:'https://i.ibb.co/WcpfKJp/8148aa2d-5a51-488a-8554-cae828ff2556-rw-1200.jpg',
+        title:'Netflix original series',
+        link:'https://www.netflix.com/bd/title/80100172'
+    },
+
+    {
+        sname: ' Starnger',
+        imgsrc:"https://i.ibb.co/k3MX4bN/,AAAABefo-ASo9-Wcqk5vr4-GCLHapgz-Gh-Rzc-X4rkc-Tvf-GPTw4h-Yopx9-Eo-WL7kahu3-KAp-Hd-VGJnp-QYHzkccf3qi3t.jpg" ,
+        title: 'A netflix series',
+        link:'https://www.netflix.com/bd/title/81001209'
+    },
+
+    {   
+        sname : 'extracurricular',
+        imgsrc : "https://i.ibb.co/wgnMmHq/AAAABZfj3-AT25-X5-O0h3mw-EO2-YLPxctxu-C-o4r-OD8-URUJx-SKFHc-ZEk-EWk-Bz-PEax2-9-PRPUWe55w-QMna-Oub-MB.jpg" ,
+        title: 'A netflix series',
+        link:'https://www.netflix.com/bd/title/80990668'  
+    },
+
+    {   
+        sname: 'The vampire Diaries',
+        imgsrc:" https://i.ibb.co/YkDd86b/vampire-diaries-cover-1485275999.jpg" ,
+        title:'A IMDB series',
+        link:'https://www.imdb.com/title/tt1405406/'  
+    },
+    {   
+        sname: 'My first first love ',
+        imgsrc:"https://i.ibb.co/hFLnYNV/MFFL-S2-Vertical-Main-EN-PRE-CMYK.jpg" ,
+        title: 'A IMDB series',
+        link:'https://www.netflix.com/bd/title/81026700'  
+    },
+    {   
+        sname: 'The vampire Diaries',
+        imgsrc:" https://i.ibb.co/YkDd86b/vampire-diaries-cover-1485275999.jpg" ,
+        title:'A IMDB series',
+        link:'https://www.imdb.com/title/tt1405406/'  
+    },
+    {
+        sname: ' Starnger',
+        imgsrc:"https://i.ibb.co/k3MX4bN/,AAAABefo-ASo9-Wcqk5vr4-GCLHapgz-Gh-Rzc-X4rkc-Tvf-GPTw4h-Yopx9-Eo-WL7kahu3-KAp-Hd-VGJnp-QYHzkccf3qi3t.jpg" ,
+        title: 'A netflix series',
+        link:'https://www.netflix.com/bd/title/81001209'
+    },
+    
+    {   
+        sname: 'The vampire Diaries',
+        imgsrc:" https://i.ibb.co/YkDd86b/vampire-diaries-cover-1485275999.jpg" ,
+        title:'A IMDB series',
+        link:'https://www.imdb.com/title/tt1405406/'  
+    }
+]
+
+    export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './Cards'
+import './index.css'
+import Sdata from './Sdata'
+
+// console.log(Sdata[1].sname)
+
+ var card =(value)=> {
+     return(
+        <Card 
+        imgsrc={value.imgsrc}
+        title={value.title}
+        sname={value.sname}
+        link={value.link} 
+        />
+     );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <>
+        <h1 className='heading-style'>List of best 5 netflix series</h1>
+ 
+        {Sdata.map(card)}
+
+    </>
+    , document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
